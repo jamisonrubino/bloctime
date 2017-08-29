be99f071ed9b8e43b6927e2d959e3cad32c605d9
jamisonrubino <jamison.rubino@gmail.com>
2017-08-28 22:55:45 -0700

    Checkpoint 2 complete. User can set work and break timers, and can only start/stop (reset) the timer.

--- app/scripts/services/Timer.js
+++ app/scripts/services/Timer.js
@@ -1,19 +1,47 @@
 (function() {
   function Timer($interval) {
     var Timer = {};
+    Timer.startTime = 1500;
     Timer.time = 1500;
-    var buttonContainer = $("#button-container");
+    var button = $(".timer-button");
 
     Timer.start = function() {
       Timer.started = $interval(function() {Timer.time-=1;}, 1000);
-      buttonContainer.html("<a ng-click=\"landing.Timer.stop()\" id=\"timer-button\">Stop</a>");
+      $("#start").addClass("hide");
+      $("#stop").removeClass("hide");
+      // buttonContainer.html("<a ng-click=\"landing.Timer.stop()\" id=\"timer-button\">Stop</a>");
+    };
+
+    Timer.reset = function() {
+      $interval.cancel(Timer.started);
+      Timer.time = Timer.startTime;
+      $("#stop").addClass("hide");
+      $("#start").removeClass("hide")
     };
 
     Timer.stop = function() {
-        Timer.time = 1500;
-        $interval.cancel(Timer.started);
+      if (Timer.started) {
+        Timer.reset();
+        // buttonContainer.html("<a ng-click=\"landing.Timer.start()\" id=\"timer-button\">Start</a>");
+      }
+    };
 
-        buttonContainer.html("<a ng-click=\"landing.Timer.start()\" id=\"timer-button\">Start</a>");
+    Timer.break = function() {
+      if (Timer.started) {
+        Timer.startTime = 300;
+        Timer.reset();
+      } else {
+        Timer.time = 300;
+      }
+    };
+
+    Timer.work = function() {
+      if (Timer.started) {
+        Timer.startTime = 1500;
+        Timer.reset();
+      } else {
+        Timer.time = 1500;
+      }
     };
 
     return Timer;
