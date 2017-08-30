ec72748149f3b852a8a0ac572ce2bb5ee5e9b93a
jamisonrubino <jamison.rubino@gmail.com>
2017-08-30 14:42:39 -0700

    Checkpoint 8 complete.

--- app/scripts/services/Tasks.js
+++ app/scripts/services/Tasks.js
@@ -2,23 +2,20 @@
   function Tasks($firebaseArray) {
     var Tasks = {};
     var ref = firebase.database().ref();
-    var num = firebase.database().ref('numTasks/value')
     var t = $firebaseArray(ref.child("Tasks"));
-    var n = $firebaseArray(ref.child("numTasks"));
     console.log(t);
-    console.log(n);
 
-    console.log(num.value);
     // t.orderByChild('id')
 
     Tasks.newTask = function() {
       var task = $("#new-task-input");
       if (task.val().length > 0) {
-        ref.child("Tasks").on("value", function(snapshot) {
-          Tasks.num = snapshot.numChildren();
+        ref.child("numTasks").once("value").then(function(snapshot) {
+            var num = snapshot.child("value").val();
+            ref.child("Tasks").child(task.val()).set({ "value": task.val(), "id": num+1 });
+            ref.child("numTasks").set({ "value": num+1 });
+            task.val("");
         });
-        ref.child("Tasks").child(task.val()).set({ "value": task.val(), "id": id });
-        task.val("");
       } else {
         alert("Error: You must enter the name of your todo item.");
       }
